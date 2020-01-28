ecebe106072379c6bae44478cfeec75505f5529b
Alexander Castillo <alcastic@debiastic.debiastic>
2020-01-27 20:05:29 -0500

    Combines redux reducers

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const redux = require('redux')
 const createStore = redux.createStore
+const combineReducers = redux.combineReducers
 
 const cakeActions = {
     BUY_CAKE: 'BUY_CAKE'
+}
+
+const iceCreamActions = {
+    BUY_ICECREAM: 'BUY_ICECREAM'
 }
 
 const buyCake = () => {
@@ -11,12 +16,21 @@ const buyCake = () => {
     }
 };
 
-const initialState = {
-    numOfCakes: 10,
+const buyIceCream = () => {
+    return {
+        type: iceCreamActions.BUY_ICECREAM
+    }
+}
+
+const initialCakeState = {
+    numOfCakes: 10
+}
+
+const initialIceCreamState = {
     numOfIceCreams: 50
 }
 
-const cakeReducer = (state = initialState, action) => {
+const cakeReducer = (state = initialCakeState, action) => {
     switch(action.type) {
         case cakeActions.BUY_CAKE:
             return {
@@ -28,7 +42,23 @@ const cakeReducer = (state = initialState, action) => {
     }
 }
 
-const store = createStore(cakeReducer)
+const iceCreamReducer = (state = initialIceCreamState, action) => {
+    switch(action.type) {
+        case iceCreamActions.BUY_ICECREAM:
+            return {
+                ... state,
+                numOfIceCreams: state.numOfIceCreams - 1
+            }
+        default:
+            return state
+    }
+}
+
+const rootReducer = combineReducers({
+    cake: cakeReducer,
+    iceCream: iceCreamReducer,
+})
+const store = createStore(rootReducer)
 
 console.log('initial-state =', store.getState())
 
@@ -40,5 +70,7 @@ store.dispatch(buyCake())
 store.dispatch(buyCake())
 store.dispatch(buyCake())
 store.dispatch(buyCake())
+store.dispatch(buyIceCream())
+store.dispatch(buyIceCream())
 
 unsubscribe()
