7fb6ee5ff17fc6e09c97d823b66cbef51c09c16d
Alexander Castillo <alcastic@debiastic.debiastic>
2020-01-27 19:43:03 -0500

    Adds redux core-elements: action, reducer, store, subscription and unsubscription

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-console.log('From index.js');
+const redux = require('redux')
+const createStore = redux.createStore
+
+const actions = {
+    BUY_CAKE: 'BUY_CAKE'
+}
+
+const buyCake = () => {
+    return {
+        type: actions.BUY_CAKE,
+        info: 'first redux action'
+    }
+};
+
+const initialState = {
+    nomOfCakes: 10
+}
+
+const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        case actions.BUY_CAKE:
+            return {
+                ... state,
+                nomOfCakes: state.nomOfCakes - 1
+            }
+        default:
+            return state
+    }
+}
+
+const store = createStore(reducer)
+
+console.log('initial-state =', store.getState())
+
+const unsubscribe = store.subscribe(
+    () => console.log('updated-state =', store.getState()))
+
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+
+unsubscribe()
